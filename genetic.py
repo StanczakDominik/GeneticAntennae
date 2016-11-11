560a12e8277ad50215a233b01e219138d977d619
Dominik Stańczak <stanczakdominik@gmail.com>
2016-11-12 00:02:57 +0100

    Add basic plotting function

--- genetic.py
+++ genetic.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 
 # grid dimensions
 XMIN = 0.
-XMAX = 10.
+XMAX = 1.
 YMIN = 0.
-YMAX = 10.
+YMAX = 1.
 
 # grid resolution
 NX = 100
@@ -36,3 +36,24 @@ def antenna_coverage(r_antenna, X_grid, Y_grid, power=1):
     # cover case where antenna is located in grid point
     result[np.isinf(result)] = 0 # TODO: find better solution
     return result
+
+def plot(coverage, r):
+    fig, ax = plt.subplots()
+    x_a, y_a = r.T
+    ax.plot(x_a, y_a, "k*", label="Antennae locations")
+
+    contours = ax.contour(X, Y, coverage, 100, cmap='viridis', label="Coverage")
+    colors = ax.contourf(X, Y, coverage, 100, cmap='viridis') #contourf: contour FILLED
+    fig.colorbar(colors)
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.legend(loc='best')
+
+    return fig
+
+
+r = np.random.random((N_ANTENNAE, 2))
+coverage = antenna_coverage(r, X, Y)
+plot(coverage, r)
+plt.show()
